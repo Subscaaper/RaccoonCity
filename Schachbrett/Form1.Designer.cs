ad5805942604149f997dadaf85e910c32fff9949
Subscaaper <63253193+Subscaaper@users.noreply.github.com>
2020-04-06 20:36:56 +0200

    Second Try

---
+++ Schachbrett/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Schachbrett
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudAnzahlZahlen = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.pnlSchachbrett = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize) (this.nudAnzahlZahlen)).BeginInit();
+            this.pnl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nudAnzahlZahlen
+            // 
+            this.nudAnzahlZahlen.Increment = new decimal(new int[] {2, 0, 0, 0});
+            this.nudAnzahlZahlen.Location = new System.Drawing.Point(30, 31);
+            this.nudAnzahlZahlen.Maximum = new decimal(new int[] {30, 0, 0, 0});
+            this.nudAnzahlZahlen.Name = "nudAnzahlZahlen";
+            this.nudAnzahlZahlen.Size = new System.Drawing.Size(52, 27);
+            this.nudAnzahlZahlen.TabIndex = 0;
+          
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(117, 29);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(163, 29);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.Black;
+            this.pnl1.Controls.Add(this.nudAnzahlZahlen);
+            this.pnl1.Controls.Add(this.btnStart);
+            this.pnl1.Location = new System.Drawing.Point(49, 42);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(492, 90);
+            this.pnl1.TabIndex = 2;
+            // 
+            // pnlSchachbrett
+            // 
+            this.pnlSchachbrett.BackColor = System.Drawing.Color.White;
+            this.pnlSchachbrett.Location = new System.Drawing.Point(49, 149);
+            this.pnlSchachbrett.Name = "pnlSchachbrett";
+            this.pnlSchachbrett.Size = new System.Drawing.Size(491, 379);
+            this.pnlSchachbrett.TabIndex = 3;
+            // 
+            // frmSchachbrett
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 562);
+            this.Controls.Add(this.pnlSchachbrett);
+            this.Controls.Add(this.pnl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmSchachbrett";
+            this.Text = "Schachbrett";
+            ((System.ComponentModel.ISupportInitialize) (this.nudAnzahlZahlen)).EndInit();
+            this.pnl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Panel pnlSchachbrett;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudAnzahlZahlen;
+    }
+}
---
+++ Taschenrechner/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddition_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = zahl1 + zahl2;
+            lblErgebnis.Text = Convert.ToString(ergebnis);
+            lblOperator.Text = "+";
+        }
+
+        private void btnSubtraktion_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = zahl1 - zahl2;
+            lblErgebnis.Text = Convert.ToString(ergebnis);
+            lblOperator.Text = "-";
+        }
+
+
+        private void btnMittelwert_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = (zahl1 + zahl2) / 2;
+            lblErgebnis.Text = Convert.ToString(ergebnis);
+            lblOperator.Text = "Durchschnitt";
+        }
+
+        private void btnMaximum_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = Math.Max(zahl1, zahl2);
+            lblErgebnis.Text = Convert.ToString(ergebnis);
+            lblOperator.Text = "Max";
+        }
+
+        private void btnPotenz_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = Math.Pow(zahl1, zahl2);
+            lblErgebnis.Text = Convert.ToString(ergebnis);
+            lblOperator.Text = "Potenz";
+        }
+
+        private void btnWörterrechnen_Click(object sender, EventArgs e)
+        {
+            string eingabe1 = txtOperand1.Text;
+            string eingabe2 = txtOperand2.Text;
+            string ergebnis = eingabe1 + eingabe2;
+            lblErgebnis.Text = Convert.ToString(ergebnis);
+            lblOperator.Text = "+";
+            
+        }
+        
+    }
+}
