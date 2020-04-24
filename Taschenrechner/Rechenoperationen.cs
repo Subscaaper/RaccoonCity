fff057493e0c26fc99f1b4715e266ba328b966da
Subscaaper <63253193+Subscaaper@users.noreply.github.com>
2020-04-24 17:39:10 +0200

    Taschenrechner

--- Taschenrechner/Rechenoperationen.cs
+++ Taschenrechner/Rechenoperationen.cs
@@ -5,6 +5,9 @@ namespace Taschenrechner
 {
     public class Rechenoperationen
     {
+        //Auslagerungsrechenoperationen
+        
+        //Addition
         public void Addition(TextBox Operand1, TextBox Operand2, Label Ergebnis, Label Operator)
         {
             double zahl1 = Convert.ToDouble(Operand1.Text);
@@ -13,7 +16,7 @@ namespace Taschenrechner
             Ergebnis.Text = Convert.ToString(ergebnis);
             Operator.Text = "+";
         }
-        
+        //Subtraktion
         public void Subtraktion(TextBox Operand1, TextBox Operand2, Label Ergebnis, Label Operator)
         {
             double zahl1 = Convert.ToDouble(Operand1.Text);
@@ -22,6 +25,7 @@ namespace Taschenrechner
             Ergebnis.Text = Convert.ToString(ergebnis);
             Operator.Text = "-";
         }
+        //Multiplikation
         public void Multiplikation(TextBox Operand1, TextBox Operand2, Label Ergebnis, Label Operator)
         {
             double zahl1 = Convert.ToDouble(Operand1.Text);
@@ -30,6 +34,7 @@ namespace Taschenrechner
             Ergebnis.Text = Convert.ToString(ergebnis);
             Operator.Text = "*";
         }
+        //Division
         public void Division(TextBox Operand1, TextBox Operand2, Label Ergebnis, Label Operator)
         {
             double zahl1 = Convert.ToDouble(Operand1.Text);
@@ -38,6 +43,7 @@ namespace Taschenrechner
             Ergebnis.Text = Convert.ToString(ergebnis);
             Operator.Text = "/";
         }
+        //Potenz
         public void Potenz(TextBox Operand1, TextBox Operand2, Label Ergebnis, Label Operator)
         {
             double zahl1 = Convert.ToDouble(Operand1.Text);
@@ -46,6 +52,7 @@ namespace Taschenrechner
             Ergebnis.Text = Convert.ToString(ergebnis);
             Operator.Text = "Potenz";
         }
+        //Maximum
         public void Maximum(TextBox Operand1, TextBox Operand2, Label Ergebnis, Label Operator)
         {
             double zahl1 = Convert.ToDouble(Operand1.Text);
@@ -54,6 +61,7 @@ namespace Taschenrechner
             Ergebnis.Text = Convert.ToString(ergebnis);
             Operator.Text = "Max";
         }
+        //Mittelwert
         public void Mittelwert(TextBox Operand1, TextBox Operand2, Label Ergebnis, Label Operator)
         {
             double zahl1 = Convert.ToDouble(Operand1.Text);
