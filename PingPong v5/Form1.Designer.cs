ca679aa1ec1cb0279d44d94914fd0f9bf5031d3e
Subscaaper <63253193+Subscaaper@users.noreply.github.com>
2021-06-02 12:04:36 +0200

    added changes to Ping Pong added CPU

--- PingPong v5/Form1.Designer.cs
+++ PingPong v5/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace Ping_Pong
             this.components = new System.ComponentModel.Container();
             this.picBall = new System.Windows.Forms.PictureBox();
             this.pnlSpiel = new System.Windows.Forms.Panel();
+            this.picLine = new System.Windows.Forms.PictureBox();
             this.picPlayer = new System.Windows.Forms.Panel();
             this.picCPU = new System.Windows.Forms.Panel();
             this.btnStart = new System.Windows.Forms.Button();
@@ -48,12 +49,13 @@ namespace Ping_Pong
             this.btnSetBall = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize) (this.picBall)).BeginInit();
             this.pnlSpiel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.picLine)).BeginInit();
             this.SuspendLayout();
             // 
             // picBall
             // 
             this.picBall.BackColor = System.Drawing.Color.Red;
-            this.picBall.Location = new System.Drawing.Point(46, 157);
+            this.picBall.Location = new System.Drawing.Point(45, 157);
             this.picBall.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.picBall.Name = "picBall";
             this.picBall.Size = new System.Drawing.Size(40, 40);
@@ -64,6 +66,7 @@ namespace Ping_Pong
             // 
             this.pnlSpiel.BackColor = System.Drawing.Color.Black;
             this.pnlSpiel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSpiel.Controls.Add(this.picLine);
             this.pnlSpiel.Controls.Add(this.picPlayer);
             this.pnlSpiel.Controls.Add(this.picCPU);
             this.pnlSpiel.Controls.Add(this.picBall);
@@ -72,7 +75,15 @@ namespace Ping_Pong
             this.pnlSpiel.Name = "pnlSpiel";
             this.pnlSpiel.Size = new System.Drawing.Size(652, 330);
             this.pnlSpiel.TabIndex = 1;
-           
+            // 
+            // picLine
+            // 
+            this.picLine.BackColor = System.Drawing.Color.White;
+            this.picLine.Location = new System.Drawing.Point(317, -1);
+            this.picLine.Name = "picLine";
+            this.picLine.Size = new System.Drawing.Size(10, 328);
+            this.picLine.TabIndex = 2;
+            this.picLine.TabStop = false;
             // 
             // picPlayer
             // 
@@ -86,7 +97,7 @@ namespace Ping_Pong
             // picCPU
             // 
             this.picCPU.BackColor = System.Drawing.Color.White;
-            this.picCPU.Location = new System.Drawing.Point(2, 137);
+            this.picCPU.Location = new System.Drawing.Point(2, 267);
             this.picCPU.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.picCPU.Name = "picCPU";
             this.picCPU.Size = new System.Drawing.Size(10, 60);
@@ -102,6 +113,7 @@ namespace Ping_Pong
             this.btnStart.Text = "Spiel Starten / Restart";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            this.btnStart.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnStart_KeyDown);
             // 
             // tmrSpiel
             // 
@@ -135,7 +147,7 @@ namespace Ping_Pong
             // 
             // vsbScrollbarrechts
             // 
-            this.vsbScrollbarrechts.LargeChange = 15;
+            this.vsbScrollbarrechts.LargeChange = 40;
             this.vsbScrollbarrechts.Location = new System.Drawing.Point(694, 42);
             this.vsbScrollbarrechts.Name = "vsbScrollbarrechts";
             this.vsbScrollbarrechts.Size = new System.Drawing.Size(20, 313);
@@ -211,9 +223,9 @@ namespace Ping_Pong
             this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize) (this.picBall)).EndInit();
             this.pnlSpiel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.picLine)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
@@ -233,5 +245,6 @@ namespace Ping_Pong
         private System.Windows.Forms.Button btnSetBall;
         private System.Windows.Forms.Panel picCPU;
         private System.Windows.Forms.Panel picPlayer;
+        private System.Windows.Forms.PictureBox picLine;
     }
 }
--- PingPong v5/Form1.cs
+++ PingPong v5/Form1.cs
@@ -9,20 +9,20 @@ namespace Ping_Pong
     {
         private int directionX;
         private int directionY;
-        public int CPUdirectionY;
+        public int CpuDirectionY = -3;
         private int pointsPlayer;
         private int pointsCpu;
 
         Gameover gameover = new Gameover();
         private Random rand = new Random();
 
-        //Ausführung des Programms Form1
+        //Initiating of the program Form1
         public Form1()
         {
             InitializeComponent();
         }
 
-        //wird der Timer aktiviert, Spiel startet
+        //When timer is activated, the game starts
         public void btnStart_Click(object sender, EventArgs e)
         {
             SetDirection();
@@ -33,129 +33,140 @@ namespace Ping_Pong
             pointsPlayer = 0;
             pointsCpu = 0;
             btnSetBall.Visible = true;
+
+            picCPU.Location = new Point(0, pnlSpiel.Height / 2 - picCPU.Height / 2);
             
-            picCPU.Location = new Point(1, (pnlSpiel.Height / 2 - picCPU.Height / 2));
+            picLine.Location = new Point((pnlSpiel.Width / 2) - (picLine.Width / 2), 0);
         }
 
         //Continues the game by clicking on the button, after a point is made, ball spawns in the middle
         private void btnSetBall_Click(object sender, EventArgs e)
         {
+            ContinueGame();
+        }
+
+        public void ContinueGame()
+        {
             tmrSpiel.Start();
             SetDirection();
             picBall.Location = new Point(pnlSpiel.Width / 2,
                 pnlSpiel.Height / 2);
-            
-            picCPU.Location = new Point(1, (pnlSpiel.Height / 2 - picCPU.Height / 2));
+
+            picCPU.Location = new Point(1, pnlSpiel.Height / 2 - picCPU.Height / 2);
+
+            SetCpuDirection(-3);
         }
 
+        //Setter for directionX and directionY for the ball, for a random start
         public void SetDirection()
         {
-            directionX = rand.Next(-4, 4);
-            directionY = rand.Next(-4, 4);
+            directionX = rand.Next(-6, 6);
 
-            if (directionX == 0 || directionY == 0)
+            if (!(directionX == 5 || directionX == -5))
             {
                 SetDirection();
             }
+
+            Console.WriteLine(directionX);
+
+            directionY = rand.Next(-6, 6);
+
+            if (!(directionY != 5 || directionY != -5))
+            {
+                SetDirection();
+            }
+
+            Console.WriteLine(directionY);
         }
 
+        //Setter for CpuDirectionY
         public void SetCpuDirection(int direction)
         {
-            CPUdirectionY = direction;
+            if ((pointsPlayer >= 20 && pointsPlayer <= 40) && (pointsCpu >= 0 && pointsCpu <= 40))
+            {
+                if (pointsPlayer >= 30)
+                {
+                    CpuDirectionY = 8 * direction;
+                    directionX = directionX + (directionX / 2);
+                    directionY = directionY + (directionY / 2);
+                }
+                else
+                {
+                    CpuDirectionY = 4 * direction;
+                    directionX = directionX + (directionX / 2);
+                    directionY = directionY + (directionY / 2);
+                }
+            }
+            else
+            {
+                CpuDirectionY = direction;
+            }
         }
 
-        //Im Timer läuft das Spiel ab
+        //Runtime of the game
         public void tmrSpiel_Tick(object sender, EventArgs e)
         {
-            SetCpuDirection(3);
-            picCPU.Location = new Point(1, picCPU.Location.Y + CPUdirectionY);
-            
-            //Bewegung des Balles
+            picCPU.Location = new Point(0, picCPU.Location.Y + CpuDirectionY);
+
             picBall.Location = new Point(picBall.Location.X + directionX,
                 picBall.Location.Y + directionY);
 
-            //Ball trifft auf Schläger
-            if (picBall.Location.X >= pnlSpiel.Width - picBall.Width - picPlayer.Width //>= 540
-                && picBall.Location.Y + picBall.Height >= picPlayer.Location.Y //Ball Punkt unten links gleich Y oben links von Player 
-                && picBall.Location.Y <= picPlayer.Location.Y + picPlayer.Height) //Ball Punkt oben links gleich Y unten links von Player
+
+            if (picBall.Location.X >= pnlSpiel.Width - picBall.Width - picPlayer.Width
+                && picBall.Location.Y + picBall.Height >= picPlayer.Location.Y
+                && picBall.Location.Y <= picPlayer.Location.Y + picPlayer.Height)
             {
                 directionX = -directionX;
             }
 
-            //Wenn der Ball den CPUSchläger trifft
             if (picBall.Location.X <= picCPU.Width
                 && picBall.Location.Y + picBall.Height >= picCPU.Location.Y
                 && picBall.Location.Y <= picCPU.Location.Y + picCPU.Height)
             {
-                directionX = +directionX;
+                directionX = -directionX;
             }
 
-            //Ball trifft auf linken Spielrand 
+            //Ball hits left wall 
             if (picBall.Location.X <= 0)
             {
                 tmrSpiel.Stop();
                 pointsPlayer += 10;
             }
 
-            //Ball trifft auf rechten Spielrand
+            //Ball hits right wall
             if (picBall.Location.X >= pnlSpiel.Width - picBall.Width)
             {
                 tmrSpiel.Stop();
                 pointsCpu += 10;
             }
 
-            //Ball trifft auf unteren Spielrand 
+            //Ball hits bottom
             if (picBall.Location.Y >= pnlSpiel.Height - picBall.Height)
             {
                 directionY = -directionY;
             }
 
-            //Ball trifft auf oberen Spielrand 
+            //Ball hits top
             if (picBall.Location.Y < 0)
             {
                 directionY = -directionY;
             }
 
-            /*CPU Player position, if the ball is in the top half of the panel it goes up, otherwise down*/
-            if (picBall.Location.Y >= 0 && picBall.Location.Y <= pnlSpiel.Height / 2)
-            {
-                SetCpuDirection(3);
-                picCPU.Location = new Point(1, picCPU.Location.Y - CPUdirectionY);
-            }
-
-            //Bottom half
-            if (picBall.Location.Y >= pnlSpiel.Height / 2 && picBall.Location.Y < pnlSpiel.Height)
-            {
-                SetCpuDirection(3);
-                picCPU.Location = new Point(1, picCPU.Location.Y + CPUdirectionY);
-            }
-
-            //If CPU is at the top
+            //If CPU is at the top, it goes back down
             if (picCPU.Location.Y <= 0)
             {
                 SetCpuDirection(3);
-                picCPU.Location = new Point(1, picCPU.Location.Y + directionY);
-                /*if (picBall.Location.Y >= 0 && picBall.Location.Y <= (pnlSpiel.Height - pnlSpiel.Height) + picBall.Height)
-                {
-                  SetCpuDirection(0);  
-                  picCPU.Location = new Point(1, picCPU.Location.Y + CPUdirectionY);
-                }
-                else
-                {
-                    SetCpuDirection(3); 
-                    picCPU.Location = new Point(1, picCPU.Location.Y + CPUdirectionY);
-                }*/
-                
+                picCPU.Location = new Point(0, picCPU.Location.Y + CpuDirectionY);
             }
 
-            //If CPU is at the bottom
-            if (picCPU.Location.Y + picCPU.Height >= pnlSpiel.Height)
+            //If CPU is at the bottom, it goes back up
+            if (picCPU.Location.Y >= pnlSpiel.Height - picCPU.Height)
             {
-                SetCpuDirection(3);
-                picCPU.Location = new Point(1, picCPU.Location.Y + CPUdirectionY);
+                SetCpuDirection(-3);
+                picCPU.Location = new Point(0, picCPU.Location.Y + CpuDirectionY);
             }
 
-            //Wenn 50 Punkte erreicht worden sind, dann ist das Spiel fertig, und das andere Formular öffnet sich
+            //if either Player scores 50 points, game is finished and the GameOver Form will open
             if (pointsPlayer == 50 || pointsCpu == 50)
             {
                 tmrSpiel.Stop();
@@ -169,8 +180,7 @@ namespace Ping_Pong
         }
 
         /*
-         * Schläger ganz rechts ins Panel setzen
-         * Ausgangsposition vom Schläger X und Y Koordinaten festlegen
+         * Set Player and CPU Position, X an Y Coordinates
          */
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -189,7 +199,7 @@ namespace Ping_Pong
             vsbScrollbarrechts.Value = picPlayer.Location.Y;
         }
 
-        //Scrollbar neben den Schläger setzen
+        //set Scrollbar next to Player
         private void vsbScrollbarrechts_Scroll(object sender, ScrollEventArgs e)
         {
             picPlayer.Location =
@@ -199,32 +209,13 @@ namespace Ping_Pong
                 picPlayer.Location.Y;
         }
 
-        // Spiel wird geschlossen
+        //Close Game
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
         }
 
-        //override, dass man nicht über die elemente fährt
-        protected override bool ProcessDialogKey(Keys keyData)
-        {
-            switch (keyData)
-            {
-                case Keys.Up:
-                    return true;
-                case Keys.Down:
-                    return true;
-                case Keys.Left:
-                    return true;
-                case Keys.Right:
-                    return true;
-            }
-
-            return base.ProcessDialogKey(keyData);
-        }
-
-        //Spiel pausieren und fortsetzen
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        private void btnStart_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.KeyData)
             {
@@ -233,6 +224,9 @@ namespace Ping_Pong
                     break;
                 case Keys.F:
                     tmrSpiel.Start();
+                    break;
+                case Keys.G:
+                    ContinueGame();
                     break;
             }
         }
